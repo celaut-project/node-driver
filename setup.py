884192405308807d93b4c1ca3442fc98c580a573
jossemii <josemi.bnf@gmail.com>
2021-11-25 23:12:27 +0100

    asd

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='acelautframework',
+    version='__dev__',
+
+    url='https://github.com/jossemii/GRPCBigBuffer_Client.git',
+    author='Josemi Avellana',
+    author_email='josemi.bnf@gmail.com',
+
+    py_modules=['iobigdata', ],
+    install_requires=[
+    ],
+)
