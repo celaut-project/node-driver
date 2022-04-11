7ca4ec1b078383767bfa7d3029204abb43e4957e
josemiguel <josemiguel.avellana@m.e-tecnia.es>
2022-04-11 20:22:18 +0200

    fad

--- test.py
+++ test.py
@@ -3,9 +3,10 @@
 # # psutil.virtual_memory().available en RamLocker.__init__
 
 from time import sleep
-from iobigdata import mem_manager
+from iobigdata import IOBigData, mem_manager
 from gas_manager import GasManager
 from threading import Thread
+import psutil
 
 def p1():
     with mem_manager(len=7):
@@ -23,6 +24,7 @@ def p3():
         print(3)
 
 GasManager().put_initial_ram_pool(mem_limit = 10)
+# IOBigData(ram_pool_method= lambda: psutil.virtual_memory().total) Simulacion de nodo.
 
 Thread( target=p1 ).start()
 sleep(1)
