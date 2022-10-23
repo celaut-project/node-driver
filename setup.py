d5f4cef7ec16e22135f15928f53ca3d7801e5009
jossemii <josemi.bnf@gmail.com>
2022-10-23 11:14:42 +0200

    asd

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='celautframework',
+    name='celaut_framework',
     version='0.0.1',
 
     url='https://github.com/jossemii/CelautFramework.git',
@@ -10,6 +10,7 @@ setup(
 
     py_modules=['celaut_framework'],
     install_requires=[
+        'grpcbigbuffer==0.0.1@git+https://github.com/jossemii/GRPCBigBuffer',
         'grpcio==1.48.1',
         'protobuf==3.19.4',
     ],
