6e41d4c1d092a136c6fc2a5137dc4e1dcf9d28c1
josemii <josemiguel.avellana@m.e-tecnia.es>
2023-04-02 00:30:11 +0200

    reformat file and update test.py

--- test/test.py
+++ test/test.py
@@ -3,10 +3,12 @@
 # # psutil.virtual_memory().available en RamLocker.__init__
 
 from time import sleep
-from celaut_framework.resource_manager import IOBigData, mem_manager
+from celaut_framework.resource_manager.resourcemanager import ResourceManager, mem_manager
 from threading import Thread, Lock
 
 RAM_POOL = 5
+
+
 class NodeResourcesManagerSimulator:
     def __init__(self) -> None:
         self.ram_pool = RAM_POOL
@@ -20,36 +22,39 @@ class NodeResourcesManagerSimulator:
 
 
 def p0():
-    with mem_manager(len= RAM_POOL):
+    with mem_manager(len=RAM_POOL):
         while True:
             sleep(1)
+
 
 def p1():
     with mem_manager(len=700):
         print(1)
+
 
 def p2():
     with mem_manager(len=900):
         sleep(7)
         print(2)
 
+
 def p3():
     with mem_manager(len=300):
         print(3)
 
-# IOBigData(ram_pool_method= lambda: psutil.virtual_memory().total) Simulacion de uso de la librería en nodo.
+
+# ResourceManager(ram_pool_method= lambda: psutil.virtual_memory().total) Simulacion de uso de la librería en nodo.
 
 nrms = NodeResourcesManagerSimulator()
-IOBigData(
-    ram_pool_method = lambda: RAM_POOL,
-    modify_resources = lambda l: nrms.modify_resources(l)
+ResourceManager(
+    ram_pool_method=lambda: RAM_POOL,
+    modify_resources=lambda l: nrms.modify_resources(l)
 )
 
-
-Thread( target=p0 ).start()
+Thread(target=p0).start()
 sleep(2)
-Thread( target=p1 ).start()
+Thread(target=p1).start()
 sleep(2)
-Thread( target=p2 ).start()
+Thread(target=p2).start()
 sleep(2)
-Thread( target=p3 ).start()
+Thread(target=p3).start()
