deebbc38fccfb933a54ec47de630c351c1f6c568
jossemii <josemi.bnf@gmail.com>
2023-08-24 21:15:20 +0200

    ...

---
+++ build/lib/node_driver/gateway/utils.py
@@ -0,0 +1,9 @@
+from node_driver.gateway.protos import gateway_pb2
+
+
+def to_gas_amount(gas_amount: int) -> gateway_pb2.GasAmount:
+    return gateway_pb2.GasAmount(n=str(gas_amount))
+
+
+def from_gas_amount(gas_amount: gateway_pb2.GasAmount) -> int:
+    return int(gas_amount.n)
