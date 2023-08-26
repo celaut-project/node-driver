608270b8da954fb0e00b0440931eb770742f812a
jossemii <josemi.bnf@gmail.com>
2023-08-26 18:17:08 +0200

    fix gateway_pb2.py proto import problem

--- src/node_driver/gateway/protos/gateway_pb2.py
+++ src/node_driver/gateway/protos/gateway_pb2.py
@@ -10,8 +10,7 @@ from google.protobuf.internal import builder as _builder
 
 _sym_db = _symbol_database.Default()
 
-
-import celaut_pb2 as celaut__pb2
+from node_driver.gateway.protos import celaut_pb2 as celaut__pb2
 from grpcbigbuffer import buffer_pb2 as buffer__pb2
 
 
