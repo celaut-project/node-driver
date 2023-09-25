6f914077fbe891c9a5e4a5797549534c6ae2a243
jossemii <jossemii@proton.me>
2023-09-25 19:51:25 +0200

    bdist_wheel

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='node_driver',
     version='0.0.1',
 
-    url='https://github.com/jossemii/CelautFramework.git',
-    author='J.Avellana',
+    url='https://github.com/jossemii/node-driver.git',
+    author='jossemii',
     author_email='jossemii@proton.me',
 
     py_modules=['node_driver'],
     install_requires=[
-        'grpcbigbuffer@git+https://github.com/jossemii/GRPCBigBuffer',
+        'grpcbigbuffer@git+https://github.com/jossemii/grpc-bb',
     ],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
