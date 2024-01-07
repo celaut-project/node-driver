56cbb9f707cb9b1bd69a59125fc691af44f98250
Josemi <40121100+jossemii@users.noreply.github.com>
2024-01-07 17:12:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='node_driver',
     version='0.0.1',
 
-    url='https://github.com/jossemii/node-driver.git',
+    url='https://github.com/celaut-project/node-driver.git',
     author='jossemii',
     author_email='jossemii@proton.me',
 
     py_modules=['node_driver'],
     install_requires=[
-        'grpcbigbuffer@git+https://github.com/jossemii/grpc-bb',
+        'grpcbigbuffer@git+https://github.com/celaut-project/grpc-bb',
     ],
     package_dir={"": "src"},
     packages=find_packages(where="src"),
